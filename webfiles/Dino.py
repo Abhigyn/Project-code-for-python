920802b85c74c2190ee8c50ee04b3b12027699ed
Abhigyan <abhigyandeepak.yt@gmail.com>
2025-08-30 15:43:40 +0530

    Working in Dino

---
+++ webfiles/Dino.py
@@ -0,0 +1,20 @@
+import pyautogui
+from PIL import ImageGrab
+import time
+
+def hit(key):
+    pyautogui.keyDown(key)
+
+def takeScreenshot():
+    return ImageGrab.grab().convert("L")
+
+
+if __name__ == "__main__":
+    time.sleep(1)
+    image = takeScreenshot()
+    data = image.load()
+    for i in range(340, 370): 
+        for j in range(250, 300): 
+            data
+            image.show()
+            break
