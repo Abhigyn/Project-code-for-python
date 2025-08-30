9b3e03c65b911b56c987732d49eb94928c9c815a
Abhigyan <abhigyandeepak.yt@gmail.com>
2025-08-30 16:01:20 +0530

    Complte obstical detection

--- webfiles/Dino.py
+++ webfiles/Dino.py
@@ -6,15 +6,15 @@ def hit(key):
     pyautogui.keyDown(key)
 
 def takeScreenshot():
-    return ImageGrab.grab().convert("L")
-
+    return ImageGrab.grab().convert("L")  
 
 if __name__ == "__main__":
     time.sleep(1)
     image = takeScreenshot()
     data = image.load()
-    for i in range(340, 370): 
-        for j in range(250, 300): 
-            data
-            image.show()
-            break
+    for i in range(320, 380):
+        for j in range(300, 355):
+            data[i, j] = 0
+
+    image.show()  
+
