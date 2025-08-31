9442d73baf19955529c54a75caeed5c434792768
Abhigyan <abhigyandeepak.yt@gmail.com>
2025-08-31 08:38:13 +0530

    Issue bot keep pressing up key unfix

--- webfiles/Dino.py
+++ webfiles/Dino.py
@@ -1,20 +1,39 @@
 import pyautogui
 from PIL import ImageGrab
 import time
-
+pyautogui.FAILSAFE = False
 def hit(key):
-    pyautogui.keyDown(key)
+    pyautogui.press(key)
 
-def takeScreenshot():
-    return ImageGrab.grab().convert("L")  
-
+def isColide(data):
+        # for birds    
+    for i in range(370, 400):
+        for j in range(200, 310):
+         if data[i, j] <100:
+            hit("down") 
+# for catus
+        for i in range(370, 400):
+            for j in range(300, 320):
+                if data[i, j] <100:
+                     hit("up")
 if __name__ == "__main__":
-    time.sleep(1)
-    image = takeScreenshot()
-    data = image.load()
-    for i in range(320, 380):
-        for j in range(300, 355):
-            data[i, j] = 0
+    print("hey.. dino game is about to start in 3 second")
+    time.sleep(3)
+    # hit("up")
+    while True:
+        image = ImageGrab.grab().convert("L")
+        data = image.load()
+        # isColide(data)
 
-    image.show()  
+        for i in range(390, 450):
+             for j in range(300, 320):
+                 data[i, j] = 171
+
+                 for i in range(390, 400):
+                     for j in range(200, 310):
+                         data[i, j] = 0
+        image.show()
+        break     
+        
+
 
