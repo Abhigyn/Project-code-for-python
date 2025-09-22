5261f4035ffc9632eeb7281e0bbe4a094282880d
Abhigyan <abhigyandeepak.yt@gmail.com>
2025-09-22 07:15:23 +0530

    More libary added

---
+++ Web scarpying/0.py
@@ -0,0 +1,24 @@
+import requests
+import time
+from fake_useragent import UserAgent
+import random
+
+url = "https://www.flipkart.com/search?q=mobiles&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_1_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_1_0_na_na_na&as-pos=1&as-type=TRENDING&suggestionId=mobiles&requestId=b3c4ffa1-3cee-4c44-9f44-96eeb826657c"
+
+session = requests.Session(url)
+
+headers = {
+    'User-Agent': UserAgent().random,
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.google.com'
+}
+
+
+
+
+r = requests.get(url)
+
+with open ("File.html","w") as f:
+    f.write(r.text)
